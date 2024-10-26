b1ae5091eecbbd62b270e5dfd6c1fa24cc2fb018
Nicholas Martino <nmartino@structurecraft.com>
2024-10-26 14:40:31 -0700

    make it tsx

---
+++ src/component/ProjectTeam.tsx
@@ -0,0 +1,31 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Col, Container, Row } from "react-bootstrap";
+import "../Fonts.css";
+
+const ProjectTeam = () => {
+  return (
+    <Container>
+      <Row>
+        <Col sm={6}></Col>
+        <Col sm={6}>
+          <hr />
+          <h4 className="subtitle nova-mono-regular">Project Team</h4>
+          <h4 className="primary-txt ibm-plex-sans-light">
+            Alexander Schiftner <br />
+            Alexis Kotzambasis <br />
+            Colin Matthews <br />
+            Cullen Sarles <br />
+            Justin Jao <br />
+            Ka Kit Chiu <br />
+            Karen Kuo <br />
+            Konrad Zaremba <br />
+            Nicholas Martino <br />
+            Peter Zhang <br />
+          </h4>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default ProjectTeam;
