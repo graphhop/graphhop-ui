9c2e868dc39818567efb7f9665cc02f3d2375af7
Nicholas Martino <nmartino@structurecraft.com>
2024-10-26 12:24:47 -0700

    network graph

--- src/NetworkGraph2.jsx
+++ src/NetworkGraph2.jsx
@@ -1,34 +1,33 @@
-import React from 'react';
-import ForceGraph3D from 'react-force-graph-3d';
-import ForceGraph2D from 'react-force-graph-2d';
-import * as d3 from 'd3';
+import React from "react";
+import ForceGraph3D from "react-force-graph-3d";
+import ForceGraph2D from "react-force-graph-2d";
 
 const NetworkGraph2 = ({ nodes, links, is3D }) => {
-    const graphData = { nodes, links };
+  const graphData = { nodes, links };
 
-    return (
-        <div style={{ width: '100%', height: '100vh' }}>
-            {is3D ? (
-                <ForceGraph3D
-                    graphData={graphData}
-                    nodeAutoColorBy="group"
-                    linkDirectionalParticles={4}
-                    linkDirectionalParticleSpeed={d => d.value * 0.001}
-                    nodeLabel={node => `${node.id}: ${node.group}`}
-                    linkLabel={link => `Value: ${link.value}`}
-                />
-            ) : (
-                <ForceGraph2D
-                    graphData={graphData}
-                    nodeAutoColorBy="group"
-                    linkDirectionalParticles={4}
-                    linkDirectionalParticleSpeed={d => d.value * 0.001}
-                    nodeLabel={node => `${node.id}: ${node.group}`}
-                    linkLabel={link => `Value: ${link.value}`}
-                />
-            )}
-        </div>
-    );
+  return (
+    <div style={{ width: "100%", height: "100vh" }}>
+      {is3D ? (
+        <ForceGraph3D
+          graphData={graphData}
+          nodeAutoColorBy="group"
+          linkDirectionalParticles={4}
+          linkDirectionalParticleSpeed={(d) => d.value * 0.001}
+          nodeLabel={(node) => `${node.name}: ${node.group}`}
+          linkLabel={(link) => `Value: ${link.value}`}
+        />
+      ) : (
+        <ForceGraph2D
+          graphData={graphData}
+          nodeAutoColorBy="group"
+          linkDirectionalParticles={4}
+          linkDirectionalParticleSpeed={(d) => d.value * 0.001}
+          nodeLabel={(node) => `${node.id}: ${node.group}`}
+          linkLabel={(link) => `Value: ${link.value}`}
+        />
+      )}
+    </div>
+  );
 };
 
 export default NetworkGraph2;
