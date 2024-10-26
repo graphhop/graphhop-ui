8e854c5d8e89b35e72db2e82c7e6dd5e0c345ae7
Nicholas Martino <nmartino@structurecraft.com>
2024-10-26 14:42:17 -0700

    prettier config

--- src/App.tsx
+++ src/App.tsx
@@ -9,53 +9,53 @@ import ProjectTeam from "./component/ProjectTeam";
 import { links, nodes } from "./data/graphMock";
 
 function App() {
-  return (
-    <>
-      <Navbar expand="lg" className="bg-body-tertiary">
-        <Container>
-          <Col sm={6}>
-            <Navbar.Brand className="nova-mono-regular" href="#home">
-              GraphHopper
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          </Col>
-          <Col sm={6}>
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto">
-                <Nav.Link
-                  className="ibm-plex-sans-light"
-                  href="#project-description"
-                >
-                  Project Description
-                </Nav.Link>
-                <Nav.Link className="ibm-plex-sans-light" href="#network-graph">
-                  Network Graph
-                </Nav.Link>
-                <Nav.Link className="ibm-plex-sans-light" href="#project-team">
-                  Project Team
-                </Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Col>
-        </Container>
-      </Navbar>
+    return (
+        <>
+            <Navbar expand="lg" className="bg-body-tertiary">
+                <Container>
+                    <Col sm={6}>
+                        <Navbar.Brand className="nova-mono-regular" href="#home">
+                            GraphHopper
+                        </Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    </Col>
+                    <Col sm={6}>
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="me-auto">
+                                <Nav.Link
+                                    className="ibm-plex-sans-light"
+                                    href="#project-description"
+                                >
+                                    Project Description
+                                </Nav.Link>
+                                <Nav.Link className="ibm-plex-sans-light" href="#network-graph">
+                                    Network Graph
+                                </Nav.Link>
+                                <Nav.Link className="ibm-plex-sans-light" href="#project-team">
+                                    Project Team
+                                </Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Col>
+                </Container>
+            </Navbar>
 
-      {/* Section 1 */}
-      <section id="project-description" style={{ paddingTop: "3rem" }}>
-        <ProjectDescription />
-      </section>
+            {/* Section 1 */}
+            <section id="project-description" style={{ paddingTop: "3rem" }}>
+                <ProjectDescription />
+            </section>
 
-      {/* Section 2 */}
-      <section id="network-graph" style={{ paddingTop: "3rem" }}>
-        <NetworkGraph2 nodes={nodes} links={links} is3D={true} />
-      </section>
+            {/* Section 2 */}
+            <section id="network-graph" style={{ paddingTop: "3rem" }}>
+                <NetworkGraph2 nodes={nodes} links={links} is3D={true} />
+            </section>
 
-      {/* Section 3 */}
-      <section id="project-team" style={{ paddingTop: "3rem" }}>
-        <ProjectTeam />
-      </section>
-    </>
-  );
+            {/* Section 3 */}
+            <section id="project-team" style={{ paddingTop: "3rem" }}>
+                <ProjectTeam />
+            </section>
+        </>
+    );
 }
 
 export default App;
