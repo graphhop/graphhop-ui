a7279686812dd479f644edb60860bc78d4040893
Nicholas Martino <nmartino@structurecraft.com>
2024-10-26 15:28:03 -0700

    add document type

--- src/Types.ts
+++ src/Types.ts
@@ -1,16 +1,23 @@
+export interface Document {
+    id: string;
+    version: string;
+    nodes: ComponentNode[];
+    links: ComponentLink[];
+}
+
 export interface ComponentNode {
-  id: string;
-  name: string;
-  added: boolean;
-  deleted: boolean;
-  changed: boolean;
-  image: string;
+    id: string;
+    name: string;
+    added: boolean;
+    deleted: boolean;
+    changed: boolean;
+    image: string;
 }
 
 export interface ComponentLink {
-  source: string;
-  target: string;
-  value: number;
+    source: string;
+    target: string;
+    value: number;
 }
 
 export const RED = "#FF0000";
@@ -21,17 +28,14 @@ export const YELLOW = "#FFFF00";
 // this method should return a hex color for the link based on whether any of their nodes were added or removed
 // if the link touches any deleted nodes, it should be red
 // if the link touches any added nodes, it should be green
-export const getLinkColor = (
-  sourceNode: ComponentNode,
-  targetNode: ComponentNode
-) => {
-  if (sourceNode.deleted || targetNode.deleted) {
-    return RED;
-  }
+export const getLinkColor = (sourceNode: ComponentNode, targetNode: ComponentNode) => {
+    if (sourceNode.deleted || targetNode.deleted) {
+        return RED;
+    }
 
-  if (sourceNode.added || targetNode.added) {
-    return GREEN;
-  }
+    if (sourceNode.added || targetNode.added) {
+        return GREEN;
+    }
 
-  return GRAY;
+    return GRAY;
 };
