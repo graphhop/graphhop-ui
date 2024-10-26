8874659fc6a730bb102733b6f7eca282bdcaf91c
Nicholas Martino <nmartino@structurecraft.com>
2024-10-26 16:26:58 -0700

    set definition from dropdown

--- src/component/NetworkView.tsx
+++ src/component/NetworkView.tsx
@@ -6,15 +6,22 @@ import { GhDefinition } from "../Types";
 type NetworkViewProps = {
     definitions: GhDefinition[];
     definitionIndex: number;
+    changeDefinition?: (definition: GhDefinition) => void;
 };
 
-export const NetworkView = ({ definitions, definitionIndex }: NetworkViewProps) => {
+export const NetworkView = (props: NetworkViewProps) => {
+    const { definitions, definitionIndex, changeDefinition } = props;
     const definition = definitions[definitionIndex];
     return (
         <Container>
             <div style={{ width: "300px", paddingBottom: "10px" }}>
                 <div>Select a version</div>
-                <DropdownList data={definitions} value={definition} textField="version" />
+                <DropdownList
+                    data={definitions}
+                    value={definition}
+                    textField="version"
+                    onChange={changeDefinition}
+                />
             </div>
             <NetworkGraph2 nodes={definition.nodes} links={definition.links} is3D={true} />
         </Container>
