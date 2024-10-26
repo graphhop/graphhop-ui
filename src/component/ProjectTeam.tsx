73e444f477cdcb2e22b73cd0631d84e475f245dc
Nicholas Martino <nmartino@structurecraft.com>
2024-10-26 16:14:26 -0700

    align project team

--- src/component/ProjectTeam.tsx
+++ src/component/ProjectTeam.tsx
@@ -6,7 +6,7 @@ const ProjectTeam = () => {
     return (
         <Container>
             <Row>
-                <Col sm={6}></Col>
+                <Col sm={3}></Col>
                 <Col sm={6}>
                     <hr />
                     <h4 className="subtitle nova-mono-regular">Project Team</h4>
